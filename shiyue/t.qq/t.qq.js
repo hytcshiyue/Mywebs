5e66a5feacca0a5cd78108027fec99c99f46fcc2
sy1445307837 <sy1445307837@163.com>
2014-08-21 16:53:53 +0800

    2014-08-21下午
    继续完成腾讯微博的细节部分

--- shiyue/t.qq/t.qq.js
+++ shiyue/t.qq/t.qq.js
@@ -27,7 +27,76 @@ $(function(){
     $(".yingyongContent").hide();
       $(".yingyong").css("background-color",""); 
     });
- 
+ //"时悦"下面晃出下拉框
+ $(".shiyue").mouseover(function(){
+     $(".geren").show(); 
+});
+  $(".shiyue").mouseout(function(){
+     $(".geren").hide(); 
+});
+  //搜索框下面的选择框
+   $(".tubiaoA").click(function(){
+    $(".smartBox").show();   
+    return false;//关键是这里，阻止冒泡
+    });
+  $(".smartBox").click(function(){
+    return false;
+    });
+  $(document).click(function(){
+    $(".smartBox").hide(); 
+    });
+  //修改头像
+  $(".MyPic").mouseover(function(){
+     $(".changePic").show(); 
+});
+  $(".MyPic").mouseout(function(){
+     $(".changePic").hide(); 
+});
+  //点一串小图标会出现窗口
+   $(".medalBox1").mouseover(function(){
+     $(".q1").show(); 
+    });
+    $(".medalBox1").mouseout(function(){
+     $(".q1").hide(); 
+   });
+    $(".medalBox2").mouseover(function(){
+     $(".q2").show(); 
+    });
+    $(".medalBox2").mouseout(function(){
+     $(".q2").hide(); 
+   });
+    $(".medalBox3").mouseover(function(){
+     $(".q3").show(); 
+    });
+    $(".medalBox3").mouseout(function(){
+     $(".q3").hide(); 
+   });
+    $(".medalBox4").mouseover(function(){
+     $(".q4").show(); 
+    });
+    $(".medalBox4").mouseout(function(){
+     $(".q4").hide(); 
+   });
+    $(".medalBox5").mouseover(function(){
+     $(".q5").show(); 
+    });
+    $(".medalBox5").mouseout(function(){
+     $(".q5").hide(); 
+   });
+    $(".medalBox6").mouseover(function(){
+     $(".q6").show(); 
+    });
+    $(".medalBox6").mouseout(function(){
+     $(".q6").hide(); 
+   });
+    $(".medalBox7").mouseover(function(){
+     $(".q7").show(); 
+    });
+    $(".medalBox7").mouseout(function(){
+     $(".q7").hide(); 
+   });
+//推荐收听部分
+$().hover()
 //评论框的弹出和关闭
   $(".pinglun").click(function(){
 
